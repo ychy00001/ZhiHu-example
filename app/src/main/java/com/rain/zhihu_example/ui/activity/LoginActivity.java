2f60a07d62d1bab421d2c774d298da418e39796a
rain ychy <yangchunyu00001@163.com>
2016-03-04 10:28:13 +0800

    登录界面补充

--- app/src/main/java/com/rain/zhihu_example/ui/activity/LoginActivity.java
+++ app/src/main/java/com/rain/zhihu_example/ui/activity/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.rain.zhihu_example.ui.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import butterknife.Bind;
 import com.rain.zhihu_example.R;
 import com.rain.zhihu_example.ui.base.BaseActivity;
 
@@ -11,7 +15,12 @@ import com.rain.zhihu_example.ui.base.BaseActivity;
  *         2016/3/2
  *         11:35
  */
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+
+    @Bind(R.id.btn_sina_login) Button mBtnSinaLogin;//新浪微博登录
+    @Bind(R.id.btn_tencent_login) Button mBtnTencentLogin;//腾讯微博登录
+    @Bind(R.id.toolbar) Toolbar mToolbar;
+
     @Override
     protected int setContentLayout() {
         return R.layout.activity_login;
@@ -20,8 +29,40 @@ public class LoginActivity extends BaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        toolBarSetting();
+        initListener();
+    }
 
+    /**初始化事件监听*/
+    private void initListener() {
+        mBtnSinaLogin.setOnClickListener(this);
+        mBtnTencentLogin.setOnClickListener(this);
     }
 
 
+    /**
+     * 关于toolbar的设置
+     */
+    private void toolBarSetting() {
+        //设置左上角的返回按钮
+        mToolbar.setNavigationIcon(R.mipmap.abc_ic_ab_back_mtrl_am_alpha);
+        mToolbar.setNavigationOnClickListener(this);
+        mToolbar.setTitle("");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_sina_login:
+                //TODO 新浪微博登录
+
+                break;
+            case R.id.btn_tencent_login:
+                //TODO 腾讯微博登录
+
+                break;
+            default:
+                finish();
+        }
+    }
 }
